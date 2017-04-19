513664bb8bfc5ff6ea2ba85b03ffc4906aa02d0c
Rogerio Alves Rodrigues <rogerioalves21@gmail.com>
2017-04-19 03:25:54 +0000

    Teste codenvy

--- abdi-formulario-comum/src/main/java/abdi/formulario/cdi/Logged.java
+++ abdi-formulario-comum/src/main/java/abdi/formulario/cdi/Logged.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package abdi.formulario.cdi;
 
 import static java.lang.annotation.ElementType.METHOD;
@@ -14,7 +9,8 @@ import java.lang.annotation.Target;
 import javax.interceptor.InterceptorBinding;
 
 /**
- *
+ * Anotacao para fazer referencia a um metodo para receber logs.
+ * 
  * @author Rogerio.Rodrigues
  */
 @Inherited
