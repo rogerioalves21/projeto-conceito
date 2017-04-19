7a72063a8b9fd61de87212ec22c85129d1bc5371
Rogerio Alves Rodrigues <rogerioalves21@gmail.com>
2017-04-19 23:41:04 +0000

    test add.

---
+++ abdi-formulario-comum/src/test/java/abdi/formulario/comum/Testes.java
@@ -0,0 +1,18 @@
+package abdi.formulario.comum;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import abdi.formulario.dto.CriteriosConsultaDTO;
+
+public class Testes {
+    
+    @Test
+    public void devePopularDados() {
+        CriteriosConsultaDTO criterios = new CriteriosConsultaDTO();
+        criterios.getDados().put("codigo", 1);
+        
+        Assert.assertEquals(1, criterios.getDados().get("codigo"));
+    }
+    
+}
