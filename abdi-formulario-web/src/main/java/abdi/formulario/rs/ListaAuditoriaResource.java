53884758aaad930ee42cfbe8fe4db3ccb3073d4e
Rogerio Alves Rodrigues <rogerioalves21@gmail.com>
2017-05-01 14:16:27 +0000

    aaaa

--- abdi-formulario-web/src/main/java/abdi/formulario/rs/ListaAuditoriaResource.java
+++ abdi-formulario-web/src/main/java/abdi/formulario/rs/ListaAuditoriaResource.java
@@ -2,15 +2,22 @@ package abdi.formulario.rs;
 
 import abdi.formulario.delegate.AuditoriaDelegate;
 import abdi.formulario.dto.RequisicaoRestDTO;
+import abdi.formulario.log.AplicacaoLogger;
 import abdi.formulario.mensageria.vo.MensagemAuditoria;
+
 import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -22,6 +29,12 @@ public class ListaAuditoriaResource {
 
     @Inject
     private AuditoriaDelegate delegate;
+    
+    @Context
+    private HttpServletRequest request;
+    
+    @Context
+    private HttpServletResponse response;
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
@@ -36,6 +49,16 @@ public class ListaAuditoriaResource {
     @Produces({MediaType.APPLICATION_JSON})
     public List<Dados> listar() {
         List<Dados> dadosList = new ArrayList<Dados>();
+
+        try {
+            if (request != null) {
+                AplicacaoLogger.getLogger(getClass()).info(request.getUserPrincipal().toString());
+                request.authenticate(response);
+            }
+        } catch (Exception excecao) {
+            AplicacaoLogger.getLogger(getClass()).error("Erro ao autenticar o usuario", excecao);
+        }
+
 
         List<MensagemAuditoria> mensagens = delegate.listar();
 
